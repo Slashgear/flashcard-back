3ff05b34df552969a36f6351d4fb2495c142d86d
Slashgear <antoine395.caron@gmail.com>
2016-07-26 14:48:24 +0200

    Fix cors by using filters

--- src/main/java/com/flashcard/web/FlashcardBackApplication.java
+++ src/main/java/com/flashcard/web/FlashcardBackApplication.java
@@ -1,10 +1,13 @@
 package com.flashcard.web;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,9 +17,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @EnableSwagger2
 @Configuration
+@EnableWebMvc
 public class FlashcardBackApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
@@ -33,11 +40,17 @@ public class FlashcardBackApplication extends WebMvcConfigurerAdapter {
                 .build();
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    @Bean
+    public FilterRegistrationBean authorizationFilter(){
+        FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+        filterRegBean.setOrder(-100000);
+        filterRegBean.setFilter(new CorsFilter());
+        filterRegBean.addInitParameter("cors.allowed.headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+        filterRegBean.addInitParameter("cors.allowed.methods", "GET,POST,HEAD,OPTIONS,DELETE,PUT");
+        List<String> urlPatterns = new ArrayList<>();
+        urlPatterns.add("/*");
+        filterRegBean.setUrlPatterns(urlPatterns);
+        return filterRegBean;
     }
 
     private ApiInfo apiInfo() {
